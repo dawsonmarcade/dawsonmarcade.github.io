3be77a12018d7dfd42e7445d34571dcaae4c6a9c
Dawson <dawsonmarcade@gmail.com>
2026-01-27 20:46:17 -0600

    Finished dark mode button and adjusted links for projects on github

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function goToProject() {
     const projectURL = "https://github.com/dawsonmarcade/dawsonmarcade.github.io/tree/main/monte_carlo_simulator";
     window.location.href = projectURL;
 }
+
+
+
+/// code for dark mode button toggle
+const toggleButton = document.getElementById('themeToggle');
+const bodyElement = document.body;
+
+toggleButton.addEventListener('click', () => {
+    // toggle dark mode logic
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    // check if current theme is light, ? will make it dark if its light and light if its dark
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+    document.documentElement.setAttribute('data-theme', newTheme)
+    localStorage.setItem('theme', newTheme)
+
+})
